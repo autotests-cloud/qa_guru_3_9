a0b1bc85eb9181557c672854116c5e41b473286d
Vasenkov Stanislav <aanher@gmail.com>
2021-03-11 20:06:31 +0300

    Update TestBase.java

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -21,7 +21,7 @@ public class TestBase {
         capabilities.setCapability("enableVideo", true);
 
         Configuration.browserCapabilities = capabilities;
-        Configuration.remote = "https://user1:1234@" + System.getProperty("remote.browser.url") + ":4444/wd/hub/";
+        Configuration.remote = System.getProperty("remote.browser.url");
         Configuration.startMaximized = true;
         //        Configuration.browser = FIREFOX;
     }
